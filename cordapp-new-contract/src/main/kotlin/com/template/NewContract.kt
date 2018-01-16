6467aaa04a825f86f28ee57ee976d573f48d17d6
joeldudleyr3 <joel.dudley@r3.com>
2018-01-16 16:32:04 +0000

    NewContract new implements the required UpgradedContract interface.

--- cordapp-new-contract/src/main/kotlin/com/template/NewContract.kt
+++ cordapp-new-contract/src/main/kotlin/com/template/NewContract.kt
@@ -1,13 +1,20 @@
 package com.template
 
-import net.corda.core.contracts.Contract
 import net.corda.core.contracts.TypeOnlyCommandData
+import net.corda.core.contracts.UpgradedContract
 import net.corda.core.transactions.LedgerTransaction
 
-open class NewContract : Contract {
+// Upgraded contracts must implement the UpgradedContract interface.
+// We're not upgrading the state, so we pass the same state as the input and output state.
+open class NewContract : UpgradedContract<State, State> {
     companion object {
         val id = "com.template.NewContract"
     }
+
+    override val legacyContract = OldContract.id
+
+    // Again, we're not upgrading the state, so we leave the states unmodified.
+    override fun upgrade(state: State) = state
 
     override fun verify(tx: LedgerTransaction) {}
 
